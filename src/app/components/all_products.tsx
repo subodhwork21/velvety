957af7984073477ee37f40e5d922dda74311cff9
subodhwork21 <subodhac.work@gmail.com>
2024-06-11 12:26:15 +0545

    added swiper in categories

--- src/app/components/all_products.tsx
+++ src/app/components/all_products.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Image from "next/image";
 import SmallProductItem from "./small_productItem";
 import { Button2 } from "./button";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper/modules";
+import "swiper/css";
 
 const AllProducts = () => {
   return (
@@ -16,15 +20,15 @@ const AllProducts = () => {
           alt="arrowright"
         ></Image>
       </div>
-      <div className="lg:py-[96px] py-[64px] px-4 lg:px-[80px] gap-[98px] flex justify-center items-center lg:flex-row flex-col bg-secondarygray">
-        <div className="flex flex-col justify-left items-left w-[532px]">
+      <div className="lg:py-[96px] py-[64px] px-4 lg:px-[80px] lg:gap-[98px] gap-[40px] flex justify-center items-center lg:flex-row flex-col bg-secondarygray">
+        <div className="flex flex-col justify-left items-left max-w-[532px] lg:w-full">
           <p className="uppercase text-[28px] font-aillight text-primary pb-4">
             All Products
           </p>
-          <p className="font-ivymodeLight text-[56px] mb-10 text-primarygray">
+          <p className="font-ivymodeLight lg:text-[56px] text-[40px] mb-10 text-primarygray">
             Mild skincare & facial routine
           </p>
-          <div className="flex justify-left items-center gap-4 flex-wrap">
+          <div className="lg:flex hidden justify-left items-center gap-4 flex-wrap">
             <p className="text-primarygray text-[18px] px-5 py-2 rounded-[40px] bg-secondary">
               All needs
             </p>
@@ -77,6 +81,101 @@ const AllProducts = () => {
             <p className="text-primary text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary">
               Regenerates after UV exposure Strengthens{" "}
             </p>
+          </div>
+          <div className="lg:hidden w-full">
+            <Swiper
+              className="lg:hidden flex justify-left items-center gap-4 mx-auto w-[340px]"
+              spaceBetween={10}
+              slidesPerView={3}
+              freeMode={true}
+              modules={[FreeMode]}
+              // onSlideChange={() => console.log("slide change")}
+              // onSwiper={(swiper) => console.log(swiper)}
+            >
+              <SwiperSlide className="text-primarygray text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] bg-secondary flex justify-center center">
+                <p>All needs</p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Protect
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Regenerates
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Revitalizes
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Feeds{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Regulates{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Purifies{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Makeup Removal{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Exfoliate{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Antioxidant{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Soothes{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Smoothes skin texture{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Tones{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Anti-waste{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Hydrate{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Strengthens{" "}
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p className="text-primary text-[16px] lg:text-[18px] px-4 py-2 rounded-[40px] border-[1px] border-primary flex justify-center center">
+                  Regenerates after UV exposure Strengthens{" "}
+                </p>
+              </SwiperSlide>
+            </Swiper>
           </div>
         </div>
         <div>
